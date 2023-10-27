0697972c2432a5c6690faec2cce2b0983a671d2f
monkkeydrag228 <mirjalilovodil@gmail.com>
2023-10-27 12:51:14 +0300

    First

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-
+  
+  <my-component />
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import MonkkeyKarusel from './MonkkeyKarusel'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('my-component', MonkkeyKarusel)
+app.mount('#app')
